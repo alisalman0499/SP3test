48f96154a24f2b703d8c1189105a9822d4b845b9
axian04 <42780774+alisalman0499@users.noreply.github.com>
2025-11-20 22:04:24 +0100

    create user directory

--- src/Show.java
+++ src/Show.java
@@ -1,45 +1,45 @@
-import java.util.ArrayList;
-
-public class Show extends Media{
-    private String episodes;
-    private ArrayList<Episode> episo
-
-
-    public Show(String name, String releaseDate, String categories, double rating, String episodes){
-        super(name, releaseDate, categories, rating);
-        this.episodes = episodes;
-    }
-
-    public loadEpisodes(){
-        Episode[][] = new Episode[getEpisodeList().size()][];
-
-    }
-
-    public ArrayList<String> getEpisodeList(){
-        ArrayList<String> allEpisodes = new ArrayList<>();
-        String[] seasonEpisodes = episodes.split(",");
-        for(String s : seasonEpisodes){
-            String[] episodeNumber = s.split("-");
-            allEpisodes.add(s);
-        }
-        return allEpisodes;
-    }
-
-    public ArrayList<String> getCategories(){
-        ArrayList<String> newCategories = new ArrayList<>();
-        String[] categoryList = categories.split(",");
-        for(String c : categoryList){
-            newCategories.add(c);
-        }
-        return newCategories;
-    }
-
-    @Override
-    String getTitleName() {
-        return this.name;
-    }
-
-    public String toString(){
-        return this.name + ", " + releaseDate + ", " + categories + ", " + rating;
-    }
-}
+//import java.util.ArrayList;
+//
+//public class Show extends Media{
+//    private String episodes;
+//    private ArrayList<Episode> episodes;
+//
+//
+//    public Show(String name, String releaseDate, String categories, double rating, String episodes){
+//        super(name, releaseDate, categories, rating);
+//        this.episodes = episodes;
+//    }
+//
+//    public loadEpisodes(){
+//        Episode[][] = new Episode[getEpisodeList().size()][];
+//
+//    }
+//
+//    public ArrayList<String> getEpisodeList(){
+//        ArrayList<String> allEpisodes = new ArrayList<>();
+//        String[] seasonEpisodes = episodes.split(",");
+//        for(String s : seasonEpisodes){
+//            String[] episodeNumber = s.split("-");
+//            allEpisodes.add(s);
+//        }
+//        return allEpisodes;
+//    }
+//
+//    public ArrayList<String> getCategories(){
+//        ArrayList<String> newCategories = new ArrayList<>();
+//        String[] categoryList = categories.split(",");
+//        for(String c : categoryList){
+//            newCategories.add(c);
+//        }
+//        return newCategories;
+//    }
+//
+//    @Override
+//    String getTitleName() {
+//        return this.name;
+//    }
+//
+//    public String toString(){
+//        return this.name + ", " + releaseDate + ", " + categories + ", " + rating;
+//    }
+//}
--- src/User.java
+++ src/User.java
@@ -1,7 +1,11 @@
+import util.FileIO;
+
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Objects;
 
 public class User {
+    FileIO io = new FileIO();
     private final String userName;
     private String password;
     private ArrayList<String> watchedTitles = new ArrayList<>();
@@ -26,16 +30,31 @@ public class User {
         return userName + ", " + password; //TO-DO
     }
 
+    public void loadUser(){
+        //Create directory for the user if it doesnt exist
+        String filePath = "data/Users/" + userName;
+        System.out.println(filePath);
+        File dir = new File(filePath);
+        if (!dir.exists()) {
+            boolean created = dir.mkdirs(); // or dir.mkdir() for only one level
+            if (!created) {
+                System.err.println("Failed to create directory");
+            }
+        }
+        //Load directory for the user if it does
+        //Load files
+    }
     public void addToSaved(String title) {
         savedTitles.add(title);
+        io.saveData(savedTitles, "data/savedList.csv", "Saved Titles");
     }
-
     public void removeFromSaved(String title) {
         savedTitles.remove(title);
     }
 
     public void addToWatched(String title) {
         watchedTitles.add(title);
+        io.saveData(watchedTitles, "data/watchedList.csv", "Watched Titles");
     }
 
     public ArrayList<String> getWatched() {
@@ -45,4 +64,5 @@ public class User {
     public ArrayList<String> getSaved() {
         return savedTitles;
     }
+
 }
